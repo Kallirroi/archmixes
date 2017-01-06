a40b063565bdc83007e322a5404ec57a9c19e802
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-05 22:48:57 -0500

    layout is OK, have to move the circles to center of SVG

--- src/Title.js
+++ src/Title.js
@@ -6,13 +6,8 @@ import './skeleton.css';
 class Title extends React.Component {
 	render() {
 		return (
-        <div>
-  			  <div className="row a" style={{marginLeft: '0%'}}> </div>
-          <div className="row">
-            <div className="one column fixed" id="titlestylz">
-              <h1>Archmixes</h1>
-            </div>
-          </div>
+        <div className="title">
+          <h1>Archmixes</h1>
         </div>
 	    )
 	}
