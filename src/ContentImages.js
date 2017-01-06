6958f051924db5e40ad80fb787a0ece91a903aa0
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-05 19:21:28 -0500

    changed the cluster script

--- src/ContentImages.js
+++ src/ContentImages.js
@@ -6,11 +6,6 @@ class SvgRenderer extends Component {
   constructor() {
     super();
     this.onRef = ref => this.ref = ref;
-    this.onDrag = d3.drag().on("drag", (d) => {
-      console.log("drag")
-      d.x = d3.event.x;
-      d.y = d3.event.y;
-    });
     this.renderSvg = this.renderSvg.bind(this);
   }
 
@@ -33,94 +28,140 @@ class SvgRenderer extends Component {
 
     /*----------------------------- Main Vis --------------------*/
     let svg = d3.select(this.ref);
-
-    let padding=1.5,
-    clusterPadding = 1, // separation between different-color circles
-    maxRadius = 12;
+    let width = this.props.width;
+    let height = this.props.height;
     
-	let n = 10, // total number of nodes
-	    m = 1, // number of distinct clusters
-	    z = d3.scaleOrdinal(d3.schemeCategory20),
-	    clusters = new Array(m);
+  	let padding = 1.5, // separation between same-color nodes
+    clusterPadding = 6, // separation between different-color nodes
+    maxRadius = 20;
 
-	let nodes = d3.range(n).map(() => {
-	    let i = Math.floor(Math.random() * m),
-	        radius = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
-	        d = {cluster: i, r: radius};
-	    if (!clusters[i] || (radius > clusters[i].r)) clusters[i] = d;
-	    return d;
+
+	var n = 10, // total number of nodes
+	    m = 1; // number of distinct clusters
+
+	var color = d3.scaleSequential(d3.interpolateRainbow)
+	    .domain(d3.range(m));
+
+	// The largest node for each cluster.
+	var clusters = new Array(m);
+
+	var nodes = d3.range(n).map(function() {
+	  var i = Math.floor(Math.random() * m),
+	      r = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
+	      d = {
+	        cluster: i,
+	        radius: r,
+	        x: Math.cos(i / m * 2 * Math.PI) * 100 + width / 2 + Math.random(),
+	        y: Math.sin(i / m * 2 * Math.PI) * 100 + height / 2 + Math.random()
+	      };
+	  if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+	  return d;
 	});
 
-	let circles = svg.append('g')
-	      .datum(nodes)
-	    .selectAll('.circle')
-	      .data(d => d)
-	    .enter().append('circle')
-	      .attr('r', (d) => d.r)
-	      .attr('fill', (d) => z(d.cluster))
-	      .call(this.onDrag);
 
-	d3.forceSimulation(nodes)
-	    .velocityDecay(0.2)
-	    .force("collide", collide)
-	    .force("cluster", clustering)
-	    .on("tick", ticked);
+	var force = d3.forceSimulation()
+	  // keep entire simulation balanced around screen center
+	  .force('center', d3.forceCenter(width/2, height/2))
 
-	function ticked() {
-	    circles
-	        .attr('cx', (d) => d.x)
-	        .attr('cy', (d) => d.y);
-	}   
+	  // cluster by section
+	  .force('cluster', cluster()
+	    .strength(0.2))
 
-	function clustering(alpha) {
+	  // apply collision with padding
+	  .force('collide', d3.forceCollide(d => d.radius + padding)
+	    .strength(0))
+
+	  .on('tick', layoutTick)
+		.nodes(nodes);
+
+	var node = svg.selectAll("circle")
+	    .data(nodes)
+	  .enter().append("circle")
+	    .style("fill", function(d) { return color(d.cluster/10); })
+	  .call(d3.drag()
+	      .on("start", dragstarted)
+	      .on("drag", dragged)
+	      .on("end", dragended));
+
+	// ramp up collision strength to provide smooth transition
+	// var transitionTime = 3000;
+	// var t = d3.timer(function (elapsed) {
+	//   var dt = elapsed / transitionTime;
+	//   force.force('collide').strength(Math.pow(dt, 2) * 0.7);
+	//   if (dt >= 1.0) t.stop();
+	// });
+
+	function layoutTick(e) {
+	  node
+	      .attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; })
+	      .attr("r", function(d) { return d.radius; });
+	  force.force('collide').strength(1);
+	}
+
+	// Move d to be adjacent to the cluster node.
+	// from: https://bl.ocks.org/mbostock/7881887
+	function cluster () {
+	  var nodes,
+	    strength = 0.1;
+
+	  function force (alpha) {
+
+	    // scale + curve alpha value
+	    alpha *= strength * alpha;
+
 	    nodes.forEach(function(d) {
-	      var cluster = clusters[d.cluster];
-	      if (cluster === d) return;
-	      var x = d.x - cluster.x,
-	          y = d.y - cluster.y,
-	          l = Math.sqrt(x * x + y * y),
-	          r = 100*(d.r + cluster.r);
-	      if (l !== r) {
+				var cluster = clusters[d.cluster];
+	    	if (cluster === d) return;
+
+	      let x = d.x - cluster.x,
+	        y = d.y - cluster.y,
+	        l = Math.sqrt(x * x + y * y),
+	        r = d.radius + cluster.radius;
+
+	      if (l != r) {
 	        l = (l - r) / l * alpha;
 	        d.x -= x *= l;
 	        d.y -= y *= l;
 	        cluster.x += x;
 	        cluster.y += y;
-	      }  
-	    });
-	}
-
-	function collide(alpha) {
-	  var quadtree = d3.quadtree()
-	      .x((d) => d.x)
-	      .y((d) => d.y)
-	      .addAll(nodes);
-
-	  nodes.forEach(function(d) {
-	    var r = d.r + maxRadius + Math.max(padding, clusterPadding),
-	        nx1 = d.x - r,
-	        nx2 = d.x + r,
-	        ny1 = d.y - r,
-	        ny2 = d.y + r;
-	    quadtree.visit(function(quad, x1, y1, x2, y2) {
-
-	      if (quad.data && (quad.data !== d)) {
-	        var x = d.x - quad.data.x,
-	            y = d.y - quad.data.y,
-	            l = Math.sqrt(x * x + y * y),
-	            r = d.r + quad.data.r + (d.cluster === quad.data.cluster ? padding : clusterPadding);
-	        if (l < r) {
-	          l = (l - r) / l * alpha;
-	          d.x -= x *= l;
-	          d.y -= y *= l;
-	          quad.data.x += x;
-	          quad.data.y += y;
-	        }
 	      }
-	      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
 	    });
-	  });
+
+	  }
+
+	  force.initialize = function (_) {
+	    nodes = _;
+	  }
+
+	  force.strength = _ => {
+	    strength = _ == null ? strength : _;
+	    return force;
+	  };
+
+	  return force;
+
 	}
+
+	function dragstarted(d) {
+	  if (!d3.event.active) force.alphaTarget(0.3).restart();
+	  d3.select(this).raise().classed("active", true);
+	  force.force('collide').strength(1);
+	}
+
+	function dragged(d) {
+	  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+	  force.force('collide').strength(1);
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) force.alphaTarget(0);
+	  d3.select(this).classed("active", false);
+	  force.force('collide').strength();
+
+	}
+
+
 
 	}
 
