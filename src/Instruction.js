387c4ef6e6546d7a6e110beaff47699a63c47333
Kallirroi <kallirroi.retzepi@gmail.com>
2017-04-26 19:13:51 -0400

    cheating with button, added thick border

--- src/Instruction.js
+++ src/Instruction.js
@@ -13,7 +13,10 @@ class Instruction extends React.Component {
 		return (
         	<div className="Instruction">
 				<div className="Content">
-					<Share url={( '_' )} text={( '_' )} />
+					<Share url={ '_' } text={ '_' } size={ 'large' }/> 
+
+					<br/>
+					<br/>
 					<InstructionMobile />
 					<Timeline dataSource={{ sourceType: 'profile', screenName: 'archmixes' }} options={{ username: 'Archmixes', height: '10vh' }} />
 					<iframe src="https://docs.google.com/spreadsheets/d/1RrZKh8pdTDijKsjTITJU33WQLNjhkjU3Z5hGIVUYIO8/pubhtml?gid=211184092&amp;single=true&amp;widget=true&amp;headers=false" allowFullScreen></iframe>
